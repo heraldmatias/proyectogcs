7dc0c74e30fdc2e63d6bc0ccc92b05585137ef00
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-04-12 17:16:35 -0500

    Update N°5

---
+++ src/apps/dependencia/urls.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import patterns, url
+
+urlpatterns = patterns('dependencia.views',
+    url(r'^ministerio/$','ministerio', name='ogcs-mantenimiento-ministerio'),
+    url(r'^odp/$','odp', name='ogcs-mantenimiento-odp'),    
+    url(r'^gobernacion/$','gobernacion', name='ogcs-mantenimiento-gobernacion'),    
+)
