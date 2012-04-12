8a34e1456b746cd23a7100c562811654b82ff7e6
heraldmatias <heraldmatias.oz@gmail.com>
2012-04-12 11:17:29 -0500

    Update src/apps/ubigeo/views.py

--- src/apps/ubigeo/views.py
+++ src/apps/ubigeo/views.py
@@ -1,35 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, render_to_response, redirect
 from forms import RegionForm, ProvinciaForm
-from dependencia.forms import MinisterioForm, OdpForm, GobernacionForm
-from redessociales.forms import InformacionForm, TwitterForm, FacebookForm, TwitterDiarioForm, FacebookDiarioForm
 from django.template import RequestContext
-from usuario.models import Usuario
+from usuario.models import Usuario, Estado
 from models import Region
 
 def region(request):
+    profile = Usuario.objects.get(user = request.user)
     if request.method == 'POST': # If the form has been submitted...
-        frmregion = RegionForm(request.POST) # A form bound to the POST data
-        #profile = Usuario.objects.get(user = request.user)
+        region = Region(numreg=4,estado=Estado.objects.get(pk=1),idusuario_creac=profile.numero)        
+        frmregion = RegionForm(request.POST, instance=region) # A form bound to the POST data
         #region = Region.objects.create(
-        #region.save()
         if frmregion.is_valid():
-            return HttpResponseRedirect('/thanks/') # Redirect after POST        
-    #frmregion = RegionForm()
-    frmprovincia = ProvinciaForm()
-    frmministerio = MinisterioForm()
-    frmgobernacion = GobernacionForm()
-    frmodp = OdpForm()
-    frminformacion = InformacionForm() 
-    frmtwitter = TwitterForm()
-    frmfacebook = FacebookForm
-    frmtwitterdiario = TwitterDiarioForm()
-    frmfacebookdiario = FacebookDiarioForm()
-    print frmregion.errors
-    return render_to_response('home/home.html', {'frmregion': frmregion,'frmprovincia':frmprovincia,
-    'frmministerio': frmministerio, 'frmgobernacion':frmgobernacion, 'frmodp': frmodp,
-    'frminformacion': frminformacion, 'frmtwitter':frmtwitter, 'frmfacebook': frmfacebook,
-    'frmtwitterdiario': frmtwitterdiario, 'frmfacebookdiario':frmfacebookdiario,}, context_instance=RequestContext(request),)    
-    #return render(request,
-            #        'alumno/alumno.html', 
-             #       { 'alumno_form' : alumno_form, 'carreras' : carreras, 'conceptos' : conceptos, 'serie_numero':serie_numero})
+            frmregion.save()
+            return redirect('/home/') # Crear un parametro en home para mostrar los mensajes de exito.
+    else:        
+        frmregion = RegionForm()
+    return render_to_response('ubigeo/region.html', {'frmregion': frmregion,}, context_instance=RequestContext(request),)
