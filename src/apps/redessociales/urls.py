6adcdebf5dd161fda7e7429a0d1373072ac44b51
heraldmatias <heraldmatias.oz@gmail.com>
2012-04-13 12:06:31 -0500

    Update src/apps/redessociales/urls.py

--- src/apps/redessociales/urls.py
+++ src/apps/redessociales/urls.py
@@ -2,5 +2,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('redessociales.views',    
-    url(r'^informacion/$','informacion', name='ogcs-informacion'),    
+    url(r'^informacion/$','informacion', name='ogcs-redes-informacion'),    
+    url(r'^twitter/$','twitter', name='ogcs-twitter-informacion'),
 )
