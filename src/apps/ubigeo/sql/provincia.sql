c7d1c2a03abc0f3808f30c6c8755fea683bd8ff4
Herald Olivares <heraldo@jorge-HP-Compaq-8200-Elite-SFF-PC.(none)>
2012-06-21 19:57:03 -0500

    fixes templates

--- src/apps/ubigeo/sql/provincia.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `redesogcs`.`provincia` DROP PRIMARY KEY,
- ADD PRIMARY KEY  USING BTREE(`codigo`, `numpro`);
--- src/apps/ubigeo/sql/region.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `redesogcs`.`region` DROP PRIMARY KEY,
- ADD PRIMARY KEY  USING BTREE(`codigo`, `numreg`);
